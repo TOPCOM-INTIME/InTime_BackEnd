faf5aa644d059ad7ddcbbb9516d807b84ba4ba0d
Jae Hyeok Shin <jtiger0303@ajou.ac.kr>
2022-11-11 01:33:16 +0900

    Google API에서 위도 경도 변환 구현

---
+++ intime/src/main/java/com/topcom/intime/Controller/GoogleApiController.java
@@ -0,0 +1,26 @@
+package com.topcom.intime.Controller;
+
+import com.topcom.intime.Dto.LocationDto;
+import com.topcom.intime.service.GoogleService;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.*;
+
+@Slf4j
+@RestController
+@RequestMapping("/api")
+public class GoogleApiController {
+    private GoogleService googleService;
+    public GoogleApiController(GoogleService googleService){
+        this.googleService=googleService;
+    }
+
+    @GetMapping("/google/{useridx}")
+    public LocationDto callGoogleApi(@PathVariable int useridx, @RequestParam String address){
+        return googleService.getDatafromApi(useridx, address);
+    }
+
+    @GetMapping("/google/{useridx}/redis")
+    public Object findByUserIdx(@PathVariable int useridx){
+        return googleService.findByUserIdx(useridx);
+    }
+}
