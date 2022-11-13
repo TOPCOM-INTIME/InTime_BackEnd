c30fff8e0747ab896fa72b9f8eb0e716e4ee6313
tjdtn0219 <s_sjang@naver.com>
2022-11-13 23:35:26 +0900

    Swager Init

--- intime/src/main/java/com/topcom/intime/Config/SecurityConfig.java
+++ intime/src/main/java/com/topcom/intime/Config/SecurityConfig.java
@@ -49,6 +49,8 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
 		.access("hasRole('ROLE_USER') or hasRole('ROLE_ADMIN')")
 		.antMatchers("/admin/**")
 		.access("hasRole('ROLE_ADMIN')")
+        .antMatchers("/v2/api-docs", "/configuration/**", "/swagger*/**", "/webjars/**")
+        .permitAll()
 		.anyRequest().permitAll();
 		
 	}
