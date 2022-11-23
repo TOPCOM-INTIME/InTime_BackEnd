4e2daf73faf798e99dcce6da3027a485b2187f42
Jae Hyeok Shin <jtiger0303@ajou.ac.kr>
2022-11-23 09:43:59 +0900

    사용자 비밀번호 변경 오류 해결&이메일 검증 추가

--- intime/src/main/java/com/topcom/intime/Dto/UpdatePasswordReqDto.java
+++ intime/src/main/java/com/topcom/intime/Dto/UpdatePasswordReqDto.java
@@ -1,9 +1,13 @@
 package com.topcom.intime.Dto;
 import lombok.AllArgsConstructor;
-import lombok.Data;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
 
-@Data
+@Getter
+@Setter
 @AllArgsConstructor
+@NoArgsConstructor
 public class UpdatePasswordReqDto {
     private String newPwd;
 }
