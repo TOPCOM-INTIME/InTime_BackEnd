dff3dbbbd121ace0a4118fc7e89903552233cc99
tjdtn0219 <s_sjang@naver.com>
2022-12-03 15:52:19 +0900

    Odsay 700m 이내 예외 처리 추가

--- intime/src/main/java/com/topcom/intime/odsay/dto/OdsayResponseDto.java
+++ intime/src/main/java/com/topcom/intime/odsay/dto/OdsayResponseDto.java
@@ -1,7 +1,9 @@
 package com.topcom.intime.odsay.dto;
 
 import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Data;
+import lombok.Getter;
 import lombok.NoArgsConstructor;
 
 @Data
@@ -10,4 +12,15 @@ import lombok.NoArgsConstructor;
 public class OdsayResponseDto {
 
 	private Result result;
+	private Error error;
+	
+	@Builder
+    @AllArgsConstructor
+    @NoArgsConstructor
+    @Getter
+	public static class Error {
+		private String msg;
+		private String code;
+	}
 }
+
