fc0a6a82a0d50ab0ecebe87a38d07d5dcace5f0f
tjdtn0219 <s_sjang@naver.com>
2022-12-06 13:47:38 +0900

    초대 수정:01

--- intime/src/main/java/com/topcom/intime/service/SchedulePoolService.java
+++ intime/src/main/java/com/topcom/intime/service/SchedulePoolService.java
@@ -22,60 +22,62 @@ public class SchedulePoolService {
 
 	@Autowired
 	private SchedulePoolRepository schedulePoolRepository;
-	
+
 	@Autowired
 	private SchedulePoolMembersRepository membersRepository;
-	
+
 	@Transactional
 	public int save_pool(Schedule schedule, User user) {
 		List<Schedule> scheduleList = new ArrayList<>();
 		scheduleList.add(schedule);
-		
-		SchedulePool schedulePool = SchedulePool.builder()
-				.schedules(scheduleList)
-				.leader(user)
-				.build();
+
+		SchedulePool schedulePool = SchedulePool.builder().schedules(scheduleList).leader(user).build();
 		return schedulePoolRepository.save(schedulePool).getId();
 	}
-	
+
 	@Transactional
 	public List<ScheduleResDto> findSchedulesInPoolById(int pid) {
 
-		SchedulePool schedulePool = schedulePoolRepository.findById(pid)
-				.orElseThrow(()->{
-					return new IllegalArgumentException("Failed to find SchedulePool by id: " + pid);
-				});
+		SchedulePool schedulePool = schedulePoolRepository.findById(pid).orElseThrow(() -> {
+			return new IllegalArgumentException("Failed to find SchedulePool by id: " + pid);
+		});
 		List<ScheduleResDto> dtoList = new ArrayList<>();
 		for (Schedule s : schedulePool.getSchedules()) {
-			dtoList.add(
-					ScheduleResDto.builder()
-					.id(s.getId())
-					.name(s.getName())
-					.destName(s.getDestName())
-					.sourceName(s.getSourceName())
-					.build()
-					);
+			dtoList.add(ScheduleResDto.builder().id(s.getId()).name(s.getName()).destName(s.getDestName())
+					.sourceName(s.getSourceName()).build());
 		}
 		return dtoList;
 	}
-	
+
 	@Transactional
 	public List<UserResDto> findMembersInPoolByPid(int pid) {
-		List<SchedulePoolMembers> schedulePoolMembersList =  membersRepository.findAllByschedulePoolId(pid);
+		List<SchedulePoolMembers> schedulePoolMembersList = membersRepository.findAllByschedulePoolId(pid);
 		List<UserResDto> userDtoList = new ArrayList<>();
 		for (SchedulePoolMembers member : schedulePoolMembersList) {
-			UserResDto userDto = new UserResDto(member.getUser().getId(), member.getUser().getUsername(), member.getUser().getEmail());
+			UserResDto userDto = new UserResDto(member.getUser().getId(), member.getUser().getUsername(),
+					member.getUser().getEmail());
 			userDtoList.add(userDto);
 		}
 		return userDtoList;
 	}
-	
+
 	@Transactional
 	public List<ScheduleInvitationDto> findScheduleInvitations(int uid) {
 		List<Integer> poolIds = membersRepository.mFindSchedulePoolInvited("INVITING", uid);
 		System.out.println("TAGGGGG : " + poolIds);
-		List<SchedulePool> pool_List = schedulePoolRepository.findAllById(poolIds);
-		System.out.println("TAGGGGG2222 : " + pool_List);
+		List<SchedulePool> pool_List = new ArrayList<>();
+		if(poolIds.size() > 0) {
+			for(int poolId : poolIds) {
+				SchedulePool findedPool = schedulePoolRepository.findById(poolId)
+						.orElseThrow(()->{
+							return new IllegalArgumentException("Failed to find Pool By Id : " + poolId);
+						});
+				System.out.println("TAGGGGG1111 : " + findedPool);
+
+				pool_List.add(findedPool);
+			}
+			System.out.println("TAGGGGG2222 : " + pool_List);
+		}
 		List<ScheduleInvitationDto> invitationDtoList = new ArrayList<>();
 		for (SchedulePool pool : pool_List) {
 			System.out.println("TAGG33 : " + pool);
@@ -92,42 +94,41 @@ public class SchedulePoolService {
 		}
 		return invitationDtoList;
 	}
-	
+
 	@Transactional
 	public List<UserResDto> findJoinedMembersByPoolId(int pid) {
-		List<SchedulePoolMembers> schedulePoolMembersList =  membersRepository.findAllByschedulePoolId(pid);
+		List<SchedulePoolMembers> schedulePoolMembersList = membersRepository.findAllByschedulePoolId(pid);
 		List<UserResDto> userDtoList = new ArrayList<>();
 		for (SchedulePoolMembers member : schedulePoolMembersList) {
 			if (member.getStatus().equals("LEADER") || member.getStatus().equals("OK")) {
-				UserResDto userDto = new UserResDto(member.getUser().getId(), member.getUser().getUsername(), member.getUser().getEmail());
+				UserResDto userDto = new UserResDto(member.getUser().getId(), member.getUser().getUsername(),
+						member.getUser().getEmail());
 				userDtoList.add(userDto);
 			}
 		}
 		return userDtoList;
 	}
-	
+
 	@Transactional
 	public void AddMemberInSchedule(int pid, int uid, boolean is_leader) {
-		
+
 		if (is_leader == true) {
 			membersRepository.mAddLeader(pid, uid, "LEADER");
-		}
-		else {
+		} else {
 			membersRepository.mAddMember(pid, uid);
 		}
 	}
-	
+
 	@Transactional
 	public void AcceptScheduleInvite(int pid, int uid) {
 		String status = "OK";
 		membersRepository.mUpdateStatus(status, uid, pid);
 	}
-	
+
 	@Transactional
 	public void delete_schedulePoolById(int id) {
 		membersRepository.deleteByschedulePoolId(id);
 		schedulePoolRepository.deleteById(id);
 	}
-	
-	
+
 }
