9c39389886e52fc3f2fc21a6e7fcedce4e447b8c
tjdtn0219 <s_sjang@naver.com>
2022-12-10 18:41:51 +0900

    AdBanner Url Add & Random Get Method Add

--- intime/src/main/java/com/topcom/intime/repository/AdBannerRepository.java
+++ intime/src/main/java/com/topcom/intime/repository/AdBannerRepository.java
@@ -3,10 +3,15 @@ package com.topcom.intime.repository;
 import java.util.List;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
 import com.topcom.intime.model.AdBanner;
 
 public interface AdBannerRepository extends JpaRepository<AdBanner, Integer> {
 
 	List<AdBanner> findAllByadvertiserId(int aid);
+	
+	@Query(value = "SELECT * FROM AdBanner ORDER BY RAND() LIMIT 1", nativeQuery = true)
+	public AdBanner findRandomBanner();
+	
 }
